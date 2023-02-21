ceb6cf0f5bd4c3a636c86aa2e72f97f053fc7618
chiamaka28 <maduikechiamaka4@gmail.com>
2023-02-21 07:54:50 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: { 
+      colors: {
+      'light-cyan': 'hsl(193, 38%, 86%)',
+      'Neon-Green': 'hsl(150, 100%, 66%)',
+      'Grayish-Blue': 'hsl(217, 19%, 38%)',
+      'Dark-Grayish-Blue': 'hsl(217, 19%, 24%)',
+      'Dark-Blue': 'hsl(218, 23%, 16%)',
+    }
+  },
+  },
+  plugins: [],
+}
