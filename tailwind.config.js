bb6c3d56b49a1e97e84772abed20d3305996002c
chiamaka28 <maduikechiamaka4@gmail.com>
2023-02-21 11:11:07 +0100

    made some css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,14 @@ module.exports = {
     extend: { 
       colors: {
       'light-cyan': 'hsl(193, 38%, 86%)',
-      'Neon-Green': 'hsl(150, 100%, 66%)',
+      'neon-green': 'hsl(150, 100%, 66%)',
       'Grayish-Blue': 'hsl(217, 19%, 38%)',
       'Dark-Grayish-Blue': 'hsl(217, 19%, 24%)',
       'Dark-Blue': 'hsl(218, 23%, 16%)',
+    },
+    backgroundImage: {
+      'mobile-pattern': "url('/images/pattern-divider-mobile.svg')",
+      'hero-pattern': "url('/images/pattern-divider-desktop.svg')"
     }
   },
   },
